b39f5873a715b6d8f985401e83fb4f0fb8e3a0c8
Ramon Brandi <ramonbrand@gmail.com>
2021-02-24 11:25:39 -0300

    initial commit

---
+++ script2.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+clear
+
+
+echo -p "Informe seu nome....."
+read nome
+
+echo "Bem Vindo $nome"
+
+exit
---
+++ script3.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+clear
+
+
+echo "Bem vindo $LOGNAME....."
+
+echo "Seu diretorio de trabalho => $HOME"
+
+exit
