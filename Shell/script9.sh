c5597fbd6e6c3c170463943dcf9df0f70596ee46
Ramon Brandi <ramonbrand@gmail.com>
2021-07-25 11:57:07 -0300

    update

--- Shell/script9.sh
+++ Shell/script9.sh
@@ -1,12 +1,10 @@
 #!/bin/zsh
 
-#script.sh
 Login(){
 if [ "$(id -u)" != "0" ]; then
 echo
 echo -n "Digite a senha root - "
 su -c "sh script.sh"
-#su - root -c "sh /home/user/script.sh"
 else
 echo "Logado"
 fi
